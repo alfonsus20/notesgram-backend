b4a65e8860c2c7185a2d325c7603b3bb133bf04a
alfonsus20 <alfonschandrawan@gmail.com>
2022-09-04 23:29:54 +0700

    create username

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,22 +1,30 @@
-import { Controller, Get, Query, UseGuards } from '@nestjs/common';
+import { Body, Controller, Get, Post, Query, UseGuards } from '@nestjs/common';
 import { User } from '@prisma/client';
 import { JwtGuard } from '../auth/guard/jwt.guard';
 import { GetUser } from '../auth/decorators/get-user.decorator';
 import { UserService } from './user.service';
+import { CreateUsernameDto } from './dto';
 
 @Controller('user')
+@UseGuards(JwtGuard)
 export class UserController {
   constructor(private userService: UserService) {}
 
-  @UseGuards(JwtGuard)
   @Get('profile/me')
   getMyProfile(@GetUser() user: User) {
     return this.userService.getMyProfile(user);
   }
 
-  @UseGuards(JwtGuard)
-  @Get('check-username')
+  @Get('username/check')
   checkUsername(@Query('username') username: string) {
     return this.userService.checkUsername(username);
+  }
+
+  @Post('username/create')
+  createUsername(
+    @GetUser('id') userId: number,
+    @Body() dto: CreateUsernameDto,
+  ) {
+    return this.userService.createUsername(userId, dto);
   }
 }
--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,6 +1,7 @@
 import { BadRequestException, Injectable } from '@nestjs/common';
 import { User } from '@prisma/client';
 import { PrismaService } from '../prisma/prisma.service';
+import { CreateUsernameDto } from './dto';
 
 @Injectable()
 export class UserService {
@@ -24,6 +25,27 @@ export class UserService {
       }
 
       return { message: 'Username is available', data: { is_available: true } };
+    } catch (err) {
+      throw err;
+    }
+  }
+
+  async createUsername(userId: number, dto: CreateUsernameDto) {
+    try {
+      const existingUser = await this.prisma.user.findUnique({
+        where: { username: dto.username },
+      });
+
+      if (existingUser) {
+        throw new BadRequestException('Username is already taken');
+      }
+
+      const user = await this.prisma.user.update({
+        data: { username: dto.username },
+        where: { id: userId },
+      });
+
+      return { message: 'Username created', data: { username: user.username } };
     } catch (err) {
       throw err;
     }
